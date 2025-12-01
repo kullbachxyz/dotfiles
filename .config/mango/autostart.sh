d21155a53a21bbe4460522bc965fd6db950da6b8
Philipp Kullbach <philipp@kullbach.net>
2025-12-01 21:34:36 +0100

    changed bar binds in mango

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,4 +1,4 @@
 waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css >/dev/null 2>&1 &
-swaybg -i ~/walls/wall >/dev/null 2>&1 &
+#swaybg -i ~/walls/wall >/dev/null 2>&1 &
 wlsunset -l 50.9375 -L 6.9603 -T 6500 -t 3300 >/dev/null 2>&1 &
 mako >/dev/null 2>&1 &
