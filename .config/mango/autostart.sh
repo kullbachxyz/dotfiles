520c8545ae64d546d1231b7e0f3166b1acfdac72
Philipp Kullbach <philipp@kullbach.net>
2025-12-02 20:49:33 +0100

    changed background to simple color

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,4 +1,5 @@
 waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css >/dev/null 2>&1 &
 #swaybg -i ~/walls/wall >/dev/null 2>&1 &
-wlsunset -l 50.9375 -L 6.9603 -T 6500 -t 3300 >/dev/null 2>&1 &
+swaybg -c 282828 >/dev/null 2>&1 &
+#wlsunset -l 50.9375 -L 6.9603 -T 6500 -t 3300 >/dev/null 2>&1 &
 mako >/dev/null 2>&1 &
