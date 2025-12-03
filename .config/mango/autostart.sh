16d4439310d091db15a656b0ac8a00130683083e
Philipp Kullbach <philipp@kullbach.net>
2025-12-03 07:07:17 +0100

    modified autostart

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -3,3 +3,7 @@ waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css >/dev/null
 swaybg -c 282828 >/dev/null 2>&1 &
 #wlsunset -l 50.9375 -L 6.9603 -T 6500 -t 3300 >/dev/null 2>&1 &
 mako >/dev/null 2>&1 &
+
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+export QT_QPA_PLATFORMTHEME=qt6ct
