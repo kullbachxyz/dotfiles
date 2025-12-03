0ddf297eb93508f479323cdc6e81604efffe219d
Philipp Kullbach <philipp@kullbach.net>
2025-12-03 07:15:28 +0100

    modified mako colors

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,9 +1,8 @@
-waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css >/dev/null 2>&1 &
-#swaybg -i ~/walls/wall >/dev/null 2>&1 &
-swaybg -c 282828 >/dev/null 2>&1 &
-#wlsunset -l 50.9375 -L 6.9603 -T 6500 -t 3300 >/dev/null 2>&1 &
-mako >/dev/null 2>&1 &
+waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css &
+swaybg -c 282828 &
+#wlsunset -l 50.9375 -L 6.9603 -T 6500 -t 3300
+mako &
 
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-export QT_QPA_PLATFORMTHEME=qt6ct
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark" &
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark" &
+export QT_QPA_PLATFORMTHEME=qt6ct &
