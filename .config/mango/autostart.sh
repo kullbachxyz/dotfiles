0ca5e352b5a57a34f1f8408c40d96388ce21f38c
Philipp Kullbach <philipp@kullbach.net>
2026-01-14 16:00:55 +0100

    mango autostart adjustments

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,9 +1,25 @@
+# Session defaults
+export TERMINAL=alacritty
+export EDITOR=nvim
+export BROWSER=librewolf
+
+# Wayland app preferences
+export MOZ_ENABLE_WAYLAND=1
+export QT_QPA_PLATFORM=wayland
+export SDL_VIDEODRIVER=wayland
+export CLUTTER_BACKEND=wayland
+
+# Desktop/session hints
+export XDG_CURRENT_DESKTOP=wayland
+export XDG_SESSION_TYPE=wayland
+
+# Theme preferences
+export GTK_THEME=Adwaita-dark
+export QT_QPA_PLATFORMTHEME=qt6ct
+
+# UI services
 bg_color="$(awk '/@define-color[[:space:]]+wb-bg/ {gsub(/;/,"",$3); print $3; exit}' ~/.config/colors/current.css)"
 waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css &
 swaybg -c "${bg_color:-#1a1b26}" &
 #wlsunset -l 50.9375 -L 6.9603 -T 6500 -t 3300
 mako &
-
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark" &
-gsettings set org.gnome.desktop.interface color-scheme "prefer-dark" &
-export QT_QPA_PLATFORMTHEME=qt6ct &
